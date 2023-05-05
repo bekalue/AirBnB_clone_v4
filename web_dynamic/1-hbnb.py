0459327731d1154bf0fd1df54602383e408a14be
jabez kifle <111049968+jabez-abija2399@users.noreply.github.com>
2023-05-05 12:48:03 +0000

    tasks

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
-import os
-import uuid
-from flask import Flask, render_template
-
 from models import storage
+from models.state import State
+from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from models.state import State
-
-
+from os import environ
+from flask import Flask, render_template
+import uuid
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
@@ -25,19 +25,20 @@ def hbnb():
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
+
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
     amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
+
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
-    vars = {
-        'states': st_ct,
-        'amenities': amenities,
-        'places': places,
-        'cache_id': uuid.uuid4()
-    }
-    return render_template('1-hbnb.html', **vars)
+
+    return render_template('1-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places, cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
